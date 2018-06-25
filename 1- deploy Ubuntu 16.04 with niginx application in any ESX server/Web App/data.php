7bb21b75cf67b13038059c1ab6887e1bfbb2ef42
Ahmed Ibrahim <engahmedibrahim1994@gmail.com>
2018-06-26 00:52:37 +0200

    Sending data to PHP

---
+++ 1- deploy Ubuntu 16.04 with niginx application in any ESX server/Web App/data.php
@@ -0,0 +1,33 @@
+<?php
+
+$esxi = $_POST['esxi'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$vmname = $_POST['vmname'];
+$osfamily = $_POST['osfamily'];
+$guestid = $_POST['guestID'];
+$memory = $_POST['memory'];
+$memoryunit = $_POST['memoryunit'];
+$vcpu = $_POST['vcpu'];
+$hd = $_POST['hd'];
+$nic = $_POST['nic'];
+$description = $_POST['description'];
+
+echo "$esxi <br/>";
+echo "$username <br/>"; 
+echo "$password <br/>";
+echo "$vmname <br/>";
+echo "$osfamily <br/>";
+echo "$guestid <br/>";
+echo "$memory <br/>";
+echo "$memoryunit <br/>";
+echo "$vcpu <br/>";
+echo "$hd <br/>";
+echo "$nic <br/>";
+echo "$description <br/>";
+
+
+
+
+
+?>
